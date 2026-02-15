30642280014073857e841ccd01af9473b5cd3f6b
shunsuke_kaya <kedadu.cvp.3150@gmail.com>
2026-02-15 18:15:27 +0900

    Update train.py

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ model = YOLO("yolov8n.pt")
 
 # モデルの学習
 results = model.train(
-    data="cat_config.yaml",  # ステップ1で作成したyamlファイル
-    epochs=100,              # エポック数（モデルの学習回数）
-    imgsz=640,               # 画像サイズ
-    batch=8,                 # バッチサイズ
-    iou=0.5,                 # 重複検出を減らすための閾値
-    name="cat_detector_v2"   # 学習結果を保存するフォルダ名
+    data="cat_config.yaml",  
+    epochs=100,              
+    imgsz=640,               
+    batch=8,                 
+    iou=0.5,                 
+    name="cat_detector_v2"   
 )
